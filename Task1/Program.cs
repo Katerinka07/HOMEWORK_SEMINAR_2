b53b36e7eb4f6445036c43ea1503e2bac4fb90ac
Ekaterina <kat-erina-07@mail.ru>
2023-06-15 02:25:37 +0400

    Task1 Two digits

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+int InputNum(string massege)
+{
+    Console.WriteLine(massege);
+    int num = Convert.ToInt32(Console.ReadLine()!);
+    return num;
+}
+int number = InputNum("Введите трехзначное число:");
+if (number < 100 || number > 1000)
+{
+    Console.WriteLine("Вы ввели не трехзначное число, повторите ввод");
+    return;
+}
+int TwoDigits = number / 10 % 10;
+Console.WriteLine($"Вторая цифра: {TwoDigits}");
