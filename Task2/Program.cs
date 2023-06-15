043cd09dd6a805d80150f33cd132777febdb52e5
Ekaterina <kat-erina-07@mail.ru>
2023-06-16 01:34:09 +0400

    Task2 Third Digit

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 13: Напишите программу, которая выводит третью цифру заданного числа или
+сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+int InputNum(string massege)
+{
+    Console.WriteLine(massege);
+    int num = Convert.ToInt32(Console.ReadLine()!);
+    return num;
+}
+int number = InputNum("Введите целое число:");
+int result = 0;
+void NoThirdDigit(int number)
+{
+    if (number < 100)
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+}
+int ThirdDigit(int number)
+{
+    while (number > 999)
+    {
+        result = number /= 10;
+    }
+    result = number % 10;
+    return result;
+}
+
+NoThirdDigit(number);
+int thirdDigit = ThirdDigit(number);
+Console.WriteLine($"Третья цифра {result}");
--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+int DayWeek(string massege)
+{
+    Console.WriteLine(massege);
+    int num = Convert.ToInt32(Console.ReadLine()!);
+    return num;
+}
+int number = DayWeek("Введите число от 1 до 7:");
+if (number < 1 || number > 7)
+    Console.WriteLine("Такого дня недели нет");
+else
+{
+    if (number > 5)
+        Console.WriteLine("Ура, выходной день!");
+    else
+        Console.WriteLine("Рабочий день");
+}
